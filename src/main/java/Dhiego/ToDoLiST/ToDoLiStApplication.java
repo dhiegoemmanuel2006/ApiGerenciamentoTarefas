3f4836bad82d6306db8868a26478890d5ce252d3
dhiegoemmanuel2006 <dhiegoemmanuelsouza@gmail.com>
2024-09-30 18:57:31 -0300

    Feat: Aplicação principal

---
+++ src/main/java/Dhiego/ToDoLiST/ToDoLiStApplication.java
@@ -0,0 +1,17 @@
+package Dhiego.ToDoLiST;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoLiStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoLiStApplication.class, args);
+	}
+
+}
